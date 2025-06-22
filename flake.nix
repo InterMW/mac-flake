1980f2fe0a2416fe0638e32784d1ff30fb300109
Joseph Melberg <josephbmelbergthird@outlook.com>
2025-06-22 15:05:11 +0000

    feature

---
+++ flake.nix
@@ -0,0 +1,79 @@
+
+{
+  description = "Nixos flake to get mac address of wlan0";
+
+  # Nixpkgs / NixOS version to use.
+  inputs.nixpkgs.url = "nixpkgs/nixos-21.05";
+
+  outputs = { self, nixpkgs }:
+    let
+
+      # to work with older version of flakes
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+
+      # Generate a user-friendly version number.
+      version = builtins.substring 0 8 lastModifiedDate;
+
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
+
+    in
+
+    {
+
+      # A Nixpkgs overlay.
+      overlay = final: prev: {
+
+        getmac = with final; stdenv.mkDerivation rec {
+          name = "getmac-${version}";
+
+          unpackPhase = ":";
+
+          buildPhase =
+            ''
+              cat > getmac <<EOF
+              #! $SHELL
+              ifconfig wlan0 | grep -Eo ..\(\:..\){5} | tr -d :
+              EOF
+              chmod +x getmac
+            '';
+
+          installPhase =
+            ''
+              mkdir -p $out/bin
+              cp getmac $out/bin/
+            '';
+        };
+
+      };
+
+      # Provide some binary packages for selected system types.
+      packages = forAllSystems (system:
+        {
+          inherit (nixpkgsFor.${system}) getmac;
+        });
+
+      # The default package for 'nix build'. This makes sense if the
+      # flake provides only one package or there is a clear "main"
+      # package.
+      defaultPackage = forAllSystems (system: self.packages.${system}.getmac);
+
+      # A NixOS module, if applicable (e.g. if the package provides a system service).
+      nixosModules.getmac =
+        { pkgs, ... }:
+        {
+          nixpkgs.overlays = [ self.overlay ];
+
+          environment.systemPackages = [ pkgs.getmac ];
+
+          #systemd.services = { ... };
+        };
+    };
+}
+
